385df5648bef53178d99b1d462ace95d6b1bae01
root <root@iZ942k2d5ezZ.(none)>
2017-03-11 08:55:34 +0800

    update

--- helpers/CFunc.php
+++ helpers/CFunc.php
@@ -96,12 +96,13 @@ class CFunc
 				array_unshift($ips, $ip); $ip = FALSE; 
 			}
 			for ($i = 0; $i < count($ips); $i++) {
-				if (!eregi ("^(10|172\.16|192\.168)\.", $ips[$i])) {
+				if (!preg_match("/^(10|172\.16|192\.168)\./i", $ips[$i])) {
 					$ip = $ips[$i];
 					break;
 				}
 			}
-			}
+		}
+		
 		return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
 	}
 	
